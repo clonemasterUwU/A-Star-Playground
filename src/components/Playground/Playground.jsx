0165f316c664c45ad8b0d04b6bd639cfbe43c02a
clonemasterUwU <huskar.the.scared.warrior@gmail.com>
2020-11-22 18:18:04 +0700

    board change

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -1,29 +1,37 @@
 import { useState } from "react";
 import Board from "./Board";
 import Tooltip from './Tooltip'
-
+import styled from "styled-components"
 export default function Playground(){
   const [boardSize,setBoardSize] = useState([15,15]);
   const [heuristic,setHeuristic] = useState("manhattan");
   const [start,setStart] = useState(false);
   return(
-    <>
-    <Tooltip
-      boardSize={boardSize} 
-      setBoardSize={setBoardSize}
-      heuristic={heuristic}
-      setHeuristic={setHeuristic}
-      start={start}
-      setStart={setStart}
+    <Wrapper>
+      <Tooltip
+        boardSize={boardSize} 
+        setBoardSize={setBoardSize}
+        heuristic={heuristic}
+        setHeuristic={setHeuristic}
+        start={start}
+        setStart={setStart}
 
-    />
-    <Board
-      start={start}
-      setStart={setStart}
-      boardSize={boardSize}
-      heuristic={heuristic}
-    />
-    
-    </>
+      />
+      <StyledBoard
+        start={start}
+        setStart={setStart}
+        boardSize={boardSize}
+        heuristic={heuristic}
+      />
+    </Wrapper>
   )
 }
+const Wrapper = styled.div`
+ display:flex;
+ flex-direction:column;
+ align-items:center;
+`
+const StyledBoard = styled(Board)`
+  width:521px;
+  height:521px;
+`
--- src/components/Playground/Tooltip.jsx
+++ src/components/Playground/Tooltip.jsx
@@ -5,24 +5,53 @@ import styled  from 'styled-components';
 export default function Tooltip(props){
   const {boardSize,setBoardSize,heuristic,setHeuristic,setStart} = props;
   return(
-    <>
-    <RowSlider
-      aria-label="Column"
-      max={20}
-      min={5}
-      marks
-      step={1}
-      value={boardSize[0]}
-      onChange={(event,value)=>setBoardSize([value,boardSize[1]])}
-      valueLabelDisplay="auto"
-      component="span"
-    />
+    <TooltipWrapper>
+      <Row>
+        Row   
+        <RowSlider
+        aria-label="Column"
+        max={20}
+        min={5}
+        marks
+        step={1}
+        value={boardSize[0]}
+        onChange={(event,value)=>setBoardSize([value,boardSize[1]])}
+        valueLabelDisplay="auto"
+        component="span"
+        />
+      </Row>
+      <Row>
+        Column
+        <RowSlider
+        aria-label="Column"
+        max={20}
+        min={5}
+        marks
+        step={1}
+        value={boardSize[1]}
+        onChange={(event,value)=>setBoardSize([boardSize[0],value])}
+        valueLabelDisplay="auto"
+        component="span"
+        />
+      </Row>
     <Button variant="contained" onClick={()=>setStart(true)}>Start</Button>
     <Button variant="contained" onClick={()=>setBoardSize([...boardSize])}>Reset</Button>
 
-    </>
+    </TooltipWrapper>
   )
 }
+const TooltipWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  width:521px;
+`
+const Row = styled.div`
+
+  display:flex;
+  justify-content:space-between;
+`
 const RowSlider = styled(Slider)`
-  width: 300px;
+  &&{
+    width: 300px;
+  }
 `
